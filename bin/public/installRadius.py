81e92370789a057fa12f41a6620c7fb7ee65f78d
Anders Karlsson <anders@televerket.net>
2012-10-22 13:08:12 +0700

    Radius LDAP
    Removed Mysql and changed to LDAP instead for auth backend for
    FreeRadius

--- bin/public/installRadius.py
+++ bin/public/installRadius.py
@@ -34,58 +34,8 @@ SCRIPT_VERSION = 1
 def build_commands(commands):
   commands.add("install-radius",             install_freeradius, help="Install FreeRadius server on the current server.")
   commands.add("uninstall-radius",           uninstall_freeradius,           help="Uninstall Freeradius server on the current server.")
-  commands.add("install-radius-database",           install_freeradius_database,           help="Install Freeradius database on mysql server.")
-  commands.add("uninstall-radius-database",           uninstall_freeradius_database,           help="Uninstall Freeradius database on mysql server.")
+  
 
-  commands.add("radius-adduser",             add_freeradius_user, "[username, password]", help="Add a radius user.")
-  commands.add("radius-deluser",             delete_freeradius_user, "[username]", help="Delete a radius user.")
-  commands.add("radius-passwd",             change_freeradius_user, "[username, password]", help="Change radius user password.")
-  commands.add("radius-addhost",             add_radius_host, "[hostip]", help="Add a freeradius host to mysql database")
-
-
-
-def add_freeradius_user(args):
-  '''
-   Add freeradius user into the mysql database.
-
-  '''
-  app.print_verbose("Add radius user version: %d" % SCRIPT_VERSION)
-
-  if (len(args) != 3):
-    raise Exception("syco radius-adduser [username] [password]")
-
-  username=args[1]
-  password=args[2]
-  mysql_exec("INSERT INTO radius.radcheck VALUES('','%s','SHA-Password',':=',SHA('%s'))" %(username,password),True)
-  mysql_exec("INSERT INTO radius.radcheck VALUES('','%s','Expiration',':=',DATE_FORMAT(DATE_ADD(NOW(),INTERVAL 90 DAY),'%%d %%M %%Y %%H:%%i'))" %username,True)
-def delete_freeradius_user(args):
-  '''
-   Dlete freeradius user from the mysql database.
-
-  '''
-  app.print_verbose("Delete radius user version: %d" % SCRIPT_VERSION)
-
-  if (len(args) != 2):
-    raise Exception("syco radius-deluser [username]")
-
-  username=args[1]
-  mysql_exec("DELETE FROM radius.radcheck WHERE username='%s'" % username,True)
-
-
-def change_freeradius_user(args):
-  '''
-   Change freeradius user password in the mysql database.
-
-  '''
-  app.print_verbose("Change radius user password version: %d" % SCRIPT_VERSION)
-
-  if (len(args) != 3):
-    raise Exception("syco radius-passwd [username] [password]")
-
-  username=args[1]
-  password=args[2]
-  mysql_exec("UPDATE radius.radcheck SET value=SHA('%s') WHERE attribute='SHA-Password' AND username='%s'" %(password,username),True)
-  mysql_exec("UPDATE radius.radcheck SET value=DATE_FORMAT(DATE_ADD(NOW(),INTERVAL 90 DAY),'%%d %%M %%Y %%H:%%i') WHERE attribute='Expiration' AND username='%s'" %username,True)
 def install_freeradius(args):
   '''
   Install and configure the mysql-server on the local host.
@@ -100,7 +50,7 @@ def install_freeradius(args):
 
   # Install the mysql-server packages.
   if (not os.access("/usr/sbin/radiusd", os.W_OK|os.X_OK)):
-    x("yum -y install freeradius-mysql freeradius-utils")
+    x("yum -y install freeradius-utils freeradius-ldap")
 
     x("/sbin/chkconfig radiusd on ")
     if (not os.access("/usr/sbin/radiusd", os.F_OK)):
@@ -110,22 +60,26 @@ def install_freeradius(args):
   iptables.add_freeradius_chain()
   iptables.save()
   
-  
   app.print_verbose("Copying config")
   
- 
-
-  sqlconf = scOpen("/etc/raddb/sql.conf")
-  sqlconf.replace("\"localhost\"",config.general.get_mysql_primary_master_ip())
-  sqlconf.replace("\.*login =.*","    login=\"radius\"" )
-  sqlconf.replace("\"radpass\"","\"%s\"" % app.get_mysql_radius_password())
+  ldapconf = scOpen("/etc/raddb/modules/ldap")
+  ldapconf.replace("\\t*server =.*","\\tserver=\"ldaps://%s\"" % config.general.get_ldap_hostname())
+  ldapconf.replace("\\t#password = .*","\\tpassword =%s" % app.get_ldap_admin_password())
+  ldapconf.replace("\\t#identity = .*","\\tidentity = \"cn=Manager,%s\"" % config.general.get_ldap_dn())
+  ldapconf.replace("\\t#base_filter = .*","\\tbase_filter = \"(employeeType=Sysop)\"")
+  ldapconf.replace("\\tfilter = .*", "\\tfilter =\"(uid=%u)\"")
+  ldapconf.replace("\\tbasedn = .*", "\\tbasedn =\"%s\"" % config.general.get_ldap_dn())
   
-  radbconf = scOpen("/etc/raddb/radiusd.conf")
-
-  radbconf.replace(".*[#].*$INCLUDE sql.conf.*",     "         $INCLUDE sql.conf")
+  #Deal with certs
+  ldapconf.replace("\\t\\t# cacertfile.*=.*","\\t\\tcacertfile\\t= /etc/openldap/cacerts/ca.crt")
+  ldapconf.replace("\\t\\t# certfile.*=.*","\\t\\tcertfile\\t= /etc/openldap/cacerts/client.crt")
+  ldapconf.replace("\\t\\t# keyfile.*=.*","\\t\\tkeyfile\\t= /etc/openldap/cacerts/client.key")
   
-  sitesConf = scOpen("/etc/raddb/sites-enabled/default")
-  sitesConf.replace("^[#].*sql$","      sql")
+  
+
+  x("/usr/bin/awk '/^[#]\\tldap/{c++;if(c==1){sub(\"^[#]\\tldap\",\"\\tldap\")}}1' %s" % "/etc/raddb/sites-enabled/default > /etc/raddb/sites-enabled/default.tmp")
+  x("cp /etc/raddb/sites-enabled/default.tmp /etc/raddb/sites-enabled/default")
+  x("rm /etc/raddb/sites-enabled/default.tmp")
   version_obj.mark_executed()
 
 def uninstall_freeradius(args):
@@ -142,58 +96,3 @@ def uninstall_freeradius(args):
 
   version_obj = version.Version("InstallFreeRadius", SCRIPT_VERSION)
   version_obj.mark_uninstalled()
-
-
-def install_freeradius_database(args):
-  db_ret = mysql_exec("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'radius'",True)
-  if ( db_ret.strip() == "" ):
-    app.print_verbose("No database, should install database...")
-    app.print_verbose("Creating database")
-    mysql_exec("CREATE DATABASE radius",True)
-    x("cat /etc/raddb/sql/mysql/schema.sql | mysql -uroot -p%s radius" %(app.get_mysql_root_password()) )
- 
-    #mysql_exec("GRANT SELECT ON radius.* TO 'production'@'localhost'",True)
-    #mysql_exec("GRANT ALL on radius.radacct TO 'production'@'localhost'",True)
-    #mysql_exec("GRANT ALL on radius.radpostauth TO 'production'@'localhost'",True)
-  else:
-    app.print_verbose("Database already exists")
-    
-  
-def uninstall_freeradius_database(self):
-	mysql_exec('DROP database radius',True)
-	
-
-def add_radius_host(args):
-  '''
-    Add fFreeradius host to mysql database.
-
-  '''
-  app.print_verbose("Add radius host version: %d" % SCRIPT_VERSION)
-
-  if (len(args) != 2):
-    raise Exception("syco radius-addhost [hostip]")
-  
-  radip = args[1]
-  mysql_exec("GRANT SELECT ON radius.* TO 'radius'@'%s' IDENTIFIED BY '%s'" % (radip,app.get_mysql_radius_password()),True)
-  mysql_exec("GRANT ALL on radius.radacct TO 'radius'@'%s' IDENTIFIED BY '%s'" % (radip,app.get_mysql_radius_password()),True)
-  mysql_exec("GRANT ALL on radius.radpostauth TO 'radius'@'%s' IDENTIFIED BY '%s'" %(radip,app.get_mysql_radius_password()),True)
-def mysql_exec(command, with_user=False, host="127.0.0.1"):
-  '''
-  Execute a MySQL query, through the command line mysql console.
-
-  todo: Don't send password on command line.
-
-  '''
-  command = command.replace('\\', '\\\\')
-  command = command.replace('"', r'\"')
-
-  cmd="mysql "
-
-  if (host):
-    cmd+= "-h" + host + " "
-
-  if (with_user):
-    cmd+='-uroot -p"' + app.get_mysql_root_password() + '" '
-
-  return x(cmd + '-e "' + command + '"')
-
